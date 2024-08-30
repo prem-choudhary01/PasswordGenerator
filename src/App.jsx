38d9833ec3c50dc95e2b7f549752141ca7e1bd7d
prem Choudhary <159515195+prem-choudhary@users.noreply.github.com>
2024-08-30 16:15:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,7 @@ function App() {
   }, [length, numberAllowed, charAllowed, passwordGenerator]);
   return (
     <>
+{/*       This is my first time to use Github */}
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-3 my-8 bg-gray-800 text-white-800">
         <h1 className="text-white text-center"> Password Generator</h1>
         <div className="className = flex shadow rounded-lg overflow-hidden mb-4">
